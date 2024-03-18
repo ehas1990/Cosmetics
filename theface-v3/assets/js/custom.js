823d3c8c32c39fc16da7cb21fa52125c2fe001c4
ehas <ehasahamed@gmail.com>
2024-03-18 17:58:56 +0530

    create product page

--- theface-v3/assets/js/custom.js
+++ theface-v3/assets/js/custom.js
@@ -11,4 +11,16 @@ $(document).ready(function(){
       $(".fa-lock").show();
       $(".form-control[name='password']").attr("type","password");
   });
+ 
+  $(".qtybtn").click(function() {
+    var $row = $(this).closest("tr"); 
+    var proprice = parseFloat($row.find(".pro-price span").text().replace("$", ""));
+    var proqty = parseFloat($row.find(".pro-quantity input").val());
+    var prosubtotal = proprice * proqty;
+    $row.find(".pro-subtotal span").html("$" + prosubtotal.toFixed(2)); 
+});
+$(".fa-trash-o").click(function(){
+  $(this).closest("tr").hide();
+});
+  
 });
