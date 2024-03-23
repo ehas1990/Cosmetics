b6c19cd143ffa046ba56db7e9d8ef2bf553613d7
ehas1990 <ehasahamed@gmail.com>
2024-03-23 07:33:40 +0530

    validation registerpage

--- theface-v3/assets/js/custom.js
+++ theface-v3/assets/js/custom.js
@@ -1,17 +1,28 @@
 $(document).ready(function(){
-  $(".fa-lock").click(function(){
-      $(".fa-lock").hide();
-      $(".fa-unlock").show();
+  $("#lock").click(function(){
+      $("#lock").hide();
+      $("#unlock").show();
       
-      $(".form-control[name='password']").attr("type","text");
+      $("#password").attr("type","text");
   });
 
-  $(".fa-unlock").click(function(){
-      $(".fa-unlock").hide();
-      $(".fa-lock").show();
-      $(".form-control[name='password']").attr("type","password");
+  $("#unlock").click(function(){
+      $("#unlock").hide();
+      $("#lock").show();
+      $("#password").attr("type","password");
   });
- 
+  $("#con_lock").click(function(){
+    $("#con_lock").hide();
+    $("#con_unlock").show();
+    
+    $("#connew_password").attr("type","text");
+});
+
+$("#con_unlock").click(function(){
+    $("#con_unlock").hide();
+    $("#con_lock").show();
+    $("#connew_password").attr("type","password");
+});
   $(".qtybtn").click(function() {
     var $row = $(this).closest("tr"); 
     var proprice = parseFloat($row.find(".pro-price span").text().replace("$", ""));
@@ -26,11 +37,11 @@ $(".fa-trash").click(function(){
   $(this).closest("li").hide();
 });
   // pagination col
-  var itemsPerPage = 5; // Define how many items you want to display per page
-  var $items = $(".product"); // Select all elements with the item-width class
+  var itemsPerPage = 5; 
+  var $items = $(".product"); 
   
-  var itemCount = $items.length; // Get the total number of items
-  var totalPages = Math.ceil(itemCount / itemsPerPage); // Calculate the total number of pages
+  var itemCount = $items.length; 
+  var totalPages = Math.ceil(itemCount / itemsPerPage); 
 
   // Hide all items initially
   $items.hide();
@@ -47,9 +58,9 @@ $(".fa-trash").click(function(){
 
   // Handle pagination button click
   $(".pagination-button").on("click", function () {
-    var pageNumber = $(this).text(); // Get the clicked page number
-    var startIndex = (pageNumber - 1) * itemsPerPage; // Calculate the start index of items to show
-    var endIndex = startIndex + itemsPerPage; // Calculate the end index of items to show
+    var pageNumber = $(this).text(); 
+    var startIndex = (pageNumber - 1) * itemsPerPage; 
+    var endIndex = startIndex + itemsPerPage;
 
     // Hide all items
     $items.hide();
@@ -57,5 +68,50 @@ $(".fa-trash").click(function(){
     // Show items for the clicked page
     $items.slice(startIndex, endIndex).show();
   });
-  
+  $('#passwordForm').click(function(event) {
+    var newPassword = $('#password').val();
+    var confirmNewPassword = $('#connew_password').val();
+
+    if (newPassword !== confirmNewPassword) {
+        alert("Passwords do not match");
+        event.preventDefault();
+    
+    }else{
+      alert("sucess");
+    }
+});
+$('#create_account').click(function(event) {
+  var newPassword = $('#password').val();
+  var confirmNewPassword = $('#connew_password').val();
+  var email =$("#email").val();
+  var contact=$("#contact").val();
+  var user=$("#user").val()
+  if (user === '') {
+    alert('Please enter a username.');
+    return false;
+}
+if (contact === '') {
+    alert('Please enter a contact number.');
+    return false;
+}
+if (email === '') {
+    alert('Please enter an email address.');
+ 
+}
+if (newPassword === '') {
+    alert('Please enter a password.');
+
+}
+if (confirmNewPassword === '') {
+    alert('Please confirm your password.');
+ 
+}
+if (newPassword!== confirmNewPassword) {
+    alert('Passwords do not match.');
+    
+}
+
+// If all validations pass, the form will submit
+alert("sucess");
+});
 });
